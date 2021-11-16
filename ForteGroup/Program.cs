fb5e746e8474cfca87ebaf7c655bbf6b606ab48d
Jasurbek Khasanboyev <81855769+jasurbuz@users.noreply.github.com>
2021-11-16 15:03:31 +0500

    Add project files.

---
+++ ForteGroup/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ForteGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            if(num != "quit")
+            {
+                try
+                {
+                    int n = int.Parse(num);
+                    Console.WriteLine("It is " + (IsPrime(n) ? "a regular" : "not a") + " prime number");
+                    if (IsMersenne(n)) Console.WriteLine("It is Mersenne prime number");
+                    Main(args);
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid number!");
+                    Main(args);
+                }
+            }
+        }
+        static bool IsPrime(int n)
+        {
+            if (n < 2) return false;
+            for (int i = 2; i * i <= n; i++)
+                if (n % i == 0) return false;
+            return true;
+        }
+        static bool IsMersenne(int n)
+        {
+            int i = 2;
+            while (((1 << i) - 1) <= n) i++;
+
+            if (((1 << i - 1) - 1) == n || ((1 << i - 1) + 1) == n)
+                return true;
+            return false;
+        }
+    }
+}
